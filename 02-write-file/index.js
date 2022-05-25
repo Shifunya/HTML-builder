a0ea30a08085fbb1bd78bdb72f37a57622c24b3b
Dia <shifunya@gmail.com>
2022-05-25 15:02:00 +0300

    fix: 2nd doesn't save to file but at least you can type

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,8 +1,19 @@
+// import * as fs from 'fs';
 import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-
-const rl = readline.createInterface({ input, output });
-
-const answer = await rl.question('Can you pls do smth? ');
-console.log(`Is it still working? ${answer}`);
-rl.close();
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+   });
+rl.write(`(You can press "Ctrl + D" to close) \n`);
+rl.question("Why don't you type me something? ", (answer) => {
+    // typetypetype
+    // console.log('Nice letters lol');
+    
+});
+rl.on('SIGINT', () => {
+    rl.question('Quit? ', (answer) => {
+        if (answer.match(/^y(es)?$/i)) {
+            rl.end();
+        }
+    });
+});
